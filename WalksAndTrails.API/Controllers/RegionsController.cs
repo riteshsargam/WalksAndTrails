5c1e8fc8175e6c6c0104fe668292dde8162d6bc5
Ritesh <ritesh.sargam00@gmail.com>
2025-03-14 12:05:53 +0530

    Get All Regions Action Method

--- WalksAndTrails.API/Controllers/RegionsController.cs
+++ WalksAndTrails.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WalksAndTrails.API.Data;
 using WalksAndTrails.API.Models.Domain;
 
 namespace WalksAndTrails.API.Controllers
@@ -9,28 +10,19 @@ namespace WalksAndTrails.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly WalksAndTrailsDbContext dbContext;
+
+        public RegionsController(WalksAndTrailsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         // GET All Regions
-        // GET : https://localhost:portnumber/api/regions
+        // GET: https://localhost:portnumber/api/regions
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>()
-            {
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Golconda Fort",
-                    Code = "GLF",
-                    RegionImageUrl = "https://www.alltrails.com/_next/image?url=https%3A%2F%2Fwww.alltrails.com%2Fapi%2Falltrails%2Fv3%2Fmaps%2F290018000%2Fstatic_map%3Fkey%3D3p0t5s6b5g4g0e8k3c1j3w7y5c3m4t8i%26size%3D368x205%26scale%3D2&w=750&q=90"
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Durgam Cheruvu",
-                    Code = "DGC",
-                    RegionImageUrl = "https://www.alltrails.com/_next/image?url=https%3A%2F%2Fwww.alltrails.com%2Fapi%2Falltrails%2Fv3%2Fmaps%2F239559151%2Fstatic_map%3Fkey%3D3p0t5s6b5g4g0e8k3c1j3w7y5c3m4t8i%26size%3D368x205%26scale%3D2&w=750&q=90"
-                }
-            };
+            var regions = dbContext.Regions.ToList();
 
             return Ok(regions);
         }
