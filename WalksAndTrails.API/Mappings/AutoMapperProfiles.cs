c9a2773243886d4366c9390f670020763f050fcf
Ritesh <ritesh.sargam00@gmail.com>
2025-03-17 15:59:26 +0530

    Navigation Properties In Entity Framework Core

--- WalksAndTrails.API/Mappings/AutoMapperProfiles.cs
+++ WalksAndTrails.API/Mappings/AutoMapperProfiles.cs
@@ -13,6 +13,7 @@ namespace WalksAndTrails.API.Mappings
             CreateMap<UpdateRegionRequestDto, Region>().ReverseMap();
             CreateMap<AddWalkRequestDto, Walk>().ReverseMap();
             CreateMap<Walk, WalkDto>().ReverseMap();   
-            }
+            CreateMap<Difficulty, DifficultyDto>().ReverseMap();    
+        }
     }
 }
