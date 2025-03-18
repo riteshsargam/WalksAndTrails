41ce61246aec6ef6157df4527f0280f4a4d8162a
Ritesh <ritesh.sargam00@gmail.com>
2025-03-18 19:15:27 +0530

    Create Auth Controller and Register Action Method

---
+++ WalksAndTrails.API/Data/RegisterRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WalksAndTrails.API.Data
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+
+    }
+}
