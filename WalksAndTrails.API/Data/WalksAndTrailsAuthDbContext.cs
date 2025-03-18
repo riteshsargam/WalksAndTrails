a6c54dc655a731ce0b522c012e3f4d2754f45db8
Ritesh <ritesh.sargam00@gmail.com>
2025-03-18 14:45:54 +0530

    Seeding Roles

--- WalksAndTrails.API/Data/WalksAndTrailsAuthDbContext.cs
+++ WalksAndTrails.API/Data/WalksAndTrailsAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WalksAndTrails.API.Data
@@ -8,6 +9,34 @@ namespace WalksAndTrails.API.Data
         public WalksAndTrailsAuthDbContext(DbContextOptions<WalksAndTrailsAuthDbContext> options): base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "8a7dd637-72a0-492d-b785-b9875e02c665";
+            var writerRoleId = "1581ba0e-7c8b-4744-b8b6-eda97a9a1c07";
+
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
