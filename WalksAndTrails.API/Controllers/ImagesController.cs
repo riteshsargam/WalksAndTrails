f54d1d598fca8e2d635577b168cd9bf4f3815779
Ritesh <ritesh.sargam00@gmail.com>
2025-03-19 12:32:48 +0530

    ImageController and Action Method for Image Upload

---
+++ WalksAndTrails.API/Controllers/ImagesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalksAndTrails.API.Models.DTO;
+
+namespace WalksAndTrails.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        // POST : api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+
+            if(ModelState.IsValid)
+            {
+                // User Repository to upload image
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+
+            if (allowedExtensions.Contains(Path.GetExtension(request.FileName)))
+            {
+                ModelState.AddModelError("File", "Unsupported file extension");
+            }
+
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("File", "File size exceeds 10MB");
+            }
+        }
+
+    }
+}
